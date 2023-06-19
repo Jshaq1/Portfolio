7ca4808bc6fdb3a0e577280fe207480998ed604a
Jordan <Jordanshaqallen@gmail.com>
2023-06-19 15:08:35 +0800

    first push

---
+++ src/Nav/Header.js
@@ -0,0 +1,18 @@
+
+
+function Header(){
+
+    return(
+        <header>
+            <a>LOGO</a>
+            <div class='hamburger-menu'></div>
+            <div class='social-links'>
+                <a><img/></a>
+                <a><img/></a>
+                <a><img/></a>
+            </div>
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/Nav/NavBar.js
@@ -0,0 +1,19 @@
+import Header from "./Header";
+
+function NavBar(){
+
+    return (
+        <nav>
+            <section class='side-menu-wrapper' >
+                <section class='side-menu-content'>
+                    {/* <Logo></Logo> */}
+                    {/* <NavLinks></NavLinks> */}
+                    {/* <ContactButton></ContactButton> */}
+                </section>
+                <Header></Header>
+            </section>
+        </nav>
+    )
+}
+
+export default NavBar;
