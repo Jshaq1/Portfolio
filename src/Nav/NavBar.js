5054451cf1eb4ffd0b3d4790fc26cdc69bbe4da7
Jordan <Jordanshaqallen@gmail.com>
2023-07-20 22:10:13 +0800

    nav edits

--- src/Nav/NavBar.js
+++ src/Nav/NavBar.js
@@ -29,8 +29,8 @@ function NavBar() {
                             <Link className='nav-links' to='#work' onClick={handleHeaderClick}>Works.</Link>
                             <Link className='nav-links' to='#about' onClick={handleHeaderClick}>About.</Link>
                             <Link className='nav-links' to='#contact' onClick={handleHeaderClick}>Contact.</Link>
-                            <a className='nav-links' target="_blank" rel="noreferrer" href='https://github.com/Jshaq1' onClick={handleHeaderClick}>Github. </a>
-                            <a className='nav-links' target="_blank" rel="noreferrer" href='https://www.linkedin.com/in/jordan-allen-36ab53149/' onClick={handleHeaderClick}>Linkedin.</a>
+                            {/* <a className='nav-links' target="_blank" rel="noreferrer" href='https://github.com/Jshaq1' onClick={handleHeaderClick}>Github. </a> */}
+                            {/* <a className='nav-links' target="_blank" rel="noreferrer" href='https://www.linkedin.com/in/jordan-allen-36ab53149/' onClick={handleHeaderClick}>Linkedin.</a> */}
                         </section>
                         <img src={BOX} alt='Box with a hand coming out reaching for a cable' />
                     </section>
